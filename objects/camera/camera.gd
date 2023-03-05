30e2528e290481ac7e1309911a80256d8f6204fb
OgGhostJelly <johnnycambodia09@gmail.com>
2023-03-05 17:01:44 +0700

    speed mult and more level

--- objects/camera/camera.gd
+++ objects/camera/camera.gd
@@ -2,7 +2,8 @@ extends Camera2D
 
 
 @export var speed: float = 40.0
+@export var fast_multiplier: float = 4.0
 
 
 func _physics_process(delta: float) -> void:
-	global_position.x += speed * delta
+	global_position.x += (speed if not Input.is_action_pressed("speed") else speed * fast_multiplier) * delta
