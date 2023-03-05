8f3b41dc3c37423860df1f3017862fa7731a1df8
OgGhostJelly <johnnycambodia09@gmail.com>
2023-03-05 22:04:34 +0700

    added a dynamic camera

--- objects/camera/camera.gd
+++ objects/camera/camera.gd
@@ -1,9 +1,17 @@
 extends Camera2D
 
 
-@export var speed: float = 40.0
-@export var fast_multiplier: float = 4.0
+@export var speed: float = 20.0
 
 
 func _physics_process(delta: float) -> void:
-	global_position.x += (speed if not Input.is_action_pressed("speed") else speed * fast_multiplier) * delta
+	global_position.x += speed * delta
+	
+	var cats: Array[Node] = get_tree().get_nodes_in_group('cats')
+	var averaged_vector: Vector2 = cats.pop_front().global_position
+	
+	for cat in cats:
+		averaged_vector = ( cat.global_position + averaged_vector ) / 2
+	
+	if averaged_vector.x > global_position.x:
+		global_position.x = averaged_vector.x
