7b221ad7fdcdae2e4bb114b673134a96abbb1038
OgGhostJelly <johnnycambodia09@gmail.com>
2023-03-05 22:33:48 +0700

    stop right side dying

--- components/visible_on_screen_killer.gd
+++ components/visible_on_screen_killer.gd
@@ -1,11 +1,15 @@
-extends VisibleOnScreenNotifier2D
-class_name VisibleOnScreenKiller2D
+extends Node2D
 
 
 @export var health: HealthComponent
 
 
-func _ready() -> void:
-	screen_exited.connect(func():
-		health.health = 0
-		)
+func _process(_delta: float) -> void:
+	var viewport_size: Vector2 = get_viewport_rect().size
+	var viewport_position: Vector2 = -get_viewport_transform().origin
+
+	if (
+		global_position.x < viewport_position.x or # exited left
+		global_position.y < viewport_position.y or # exited top
+		global_position.y > viewport_position.y + viewport_size.y # exited bottom
+		): health.health = 0
