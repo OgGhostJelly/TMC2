a6efbcdfed31ee84fbd99cfd4ceaaab9809d9dd8
OgGhostJelly <johnnycambodia09@gmail.com>
2023-03-05 09:41:53 +0700

    edited hammer and todo list and collision visibility

--- objects/obstacles/hammer/puncher.gd
+++ objects/obstacles/hammer/puncher.gd
@@ -1,8 +1,7 @@
 extends RigidBody2D
 
 
-@export var speed: float = 200.0
+@export var speed: float = 300.0
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	constant_force = Vector2.from_angle(atan2(position.y, position.x) - PI/2) * speed
-
